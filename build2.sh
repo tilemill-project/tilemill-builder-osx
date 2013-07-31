1569c0b7b22bae610443f35b18edd4e4cc0cb96a
Dane Springmeyer <dane@dbsgeo.com>
2013-07-30 21:39:19 -0400

    better output

--- build2.sh
+++ build2.sh
@@ -7,9 +7,9 @@
 THIS_BUILD_ROOT=/Volumes/Flex/mapnik-packaging/osx
 cd ${THIS_BUILD_ROOT}
 if mkdir LOCKFILE; then
-   echo no lock found, building...
+   echo 'no lock found, building'
 else
-   echo lock found, exiting…
+   echo 'lock found, exiting!'
    exit 1
 fi
 
@@ -31,6 +31,7 @@ while getopts "f" OPT; do
 done
 
 function localize_node_mapnik {
+    echo 'localizing mapnik'
     cp ${MAPNIK_BIN_SOURCE}/lib/libmapnik.dylib lib/
     cp -r ${MAPNIK_BIN_SOURCE}/lib/mapnik lib/
     install_name_tool -id libmapnik.dylib lib/libmapnik.dylib
@@ -70,20 +71,23 @@ function clean_node_modules {
 }
 
 # go
+echo 'updading mapnik-packaging checkout'
 git pull
 source MacOSX.sh
 ./scripts/download_deps.sh
 export JOBS=2
 
 # rebuild node if needed
+echo 'updading node'
 if [ ! -d ${PACKAGES}/node-v${NODE_VERSION} ] || $FORCE_BUILD; then
     ./scripts/build_node.sh
     FORCE_BUILD=true
 else
-    echo skipping node-v${NODE_VERSION} build
+    echo '  skipping node-v${NODE_VERSION} build'
 fi
 
 # rebuild mapnik if needed
+echo 'updading mapnik'
 cd ${THIS_BUILD_ROOT}/mapnik
 git describe > mapnik.describe
 git pull
@@ -92,21 +96,26 @@ if [ `git describe` != `cat mapnik.describe` ] || $FORCE_BUILD; then
     ./scripts/build_mapnik.sh
     FORCE_BUILD=true
 else
-    echo 'skipping mapnik build'
+    echo '  skipping mapnik build'
 fi
 
 function rebuild_app {
+    echo 'clearing node_modules'
     rm -rf ./node_modules
+    echo 'cleaning npm cache'
     npm cache clean
     rm -f ./node
     cp `which node` ./
+    echo 'running npm install'
     npm install --sqlite=${BUILD}
+    echo 'cleaning out uneeded items in node_modules'
     clean_node_modules
     cd ./node_modules/mapnik
     localize_node_mapnik
 }
 
 # rebuild tm2 if needed
+echo 'updading tm2'
 cd ${THIS_BUILD_ROOT}/tm2
 git rev-list --max-count=1 HEAD | cut -c 1-7 > tm2.describe
 git pull
@@ -120,17 +129,20 @@ if [ `git rev-list --max-count=1 HEAD | cut -c 1-7` != `cat tm2.describe` ] || $
     ' > start.command
     chmod +x start.command
     filename=tm2-osx-${this_day}-`cat tm2/tm2.describe`.tar.gz
+    echo 'creating $filename'
     tar czfH ${filename} \
       --exclude=.git* \
        start.command tm2
     #ditto -c -k --sequesterRsrc --keepParent --zlibCompressionLevel 9 tm2/ ${ZIP_ARCHIVE}
     UPLOAD="s3://tilemill/dev/${filename}"
+    echo 'uploading $UPLOAD'
     ./s3cmd/s3cmd --acl-public put ${filename} ${UPLOAD}
 else
-    echo 'skipping tm2 build'
+    echo '  skipping tm2 build'
 fi
 
 # rebuild tilemill if needed
+echo 'updading tilemill'
 cd ${THIS_BUILD_ROOT}/tilemill
 git describe > tilemill.describe
 git pull
@@ -166,11 +178,12 @@ if [ `git describe` != `cat tilemill.describe` ] || $FORCE_BUILD; then
     dev_version=$( git describe --tags )
     filename="TileMill-$dev_version.zip"
     UPLOAD="s3://tilemill/dev/${filename}"
+    echo 'uploading $UPLOAD'
     ../../../s3cmd/s3cmd --acl-public put TileMill.zip ${UPLOAD}
     # TODO - get sparkle private key on the machine
     #echo 'Yes' | make sparkle
 else
-    echo 'skipping tilemill build'
+    echo '  skipping tilemill build'
 fi
 
 cd ${THIS_BUILD_ROOT}
