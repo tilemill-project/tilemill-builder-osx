dd43a644153a55ff1f5d89de402a3d511ba26b4a
Dane Springmeyer <dane@dbsgeo.com>
2013-08-14 22:01:50 -0400

    make the cron exit on fatal error

--- cron2.sh
+++ cron2.sh
@@ -7,5 +7,6 @@ curl -s https://raw.github.com/mapbox/tilemill-builder-osx/master/build2.sh > $H
 # run it, saving output
 #
 source $HOME/Desktop/build2.sh
+export FATAL=true
 go 2>&1 > /Volumes/Flex/build2.log
 
