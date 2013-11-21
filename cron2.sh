0d3112181ee387997426cfa5c6588f9445c4eca1
Dane Springmeyer <dane@dbsgeo.com>
2013-11-20 17:30:10 -0800

    append to log

--- cron2.sh
+++ cron2.sh
@@ -8,5 +8,5 @@ curl -s https://raw.github.com/mapbox/tilemill-builder-osx/master/build2.sh > $H
 #
 source $HOME/Desktop/build2.sh
 export FATAL=true
-go 2>&1 > /Volumes/Flex/build2.log
+go 2>&1 >> /Volumes/Flex/build2.log
 
