b9dc72b6a25e83f564f170acc23f834af4042ea7
Dane Springmeyer <dane@mapbox.com>
2014-02-10 12:15:16 -0800

    fix typo

--- build2.sh
+++ build2.sh
@@ -142,7 +142,7 @@ function init_building {
     fi
     source MacOSX.sh
     # put mapnik-config on path
-    export PATH="${MAPNIK_BIN_SOURCE}/bin"
+    export PATH="${MAPNIK_BIN_SOURCE}/bin":$PATH
     export JOBS=2
     # set these to ensure proper linking of all c++ libs
     # we do not set them by default to avoid linking c libs to libc++
